27d58473e7c3d39a2a6be31819f8031312432c21
Patrick Narciso <patrick.ncsilva@gmail.com>
2018-09-14 00:18:58 -0300

    Creating mixins of font-face and media-queries

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,43 @@
 <template>
   <div class="home">
+    <banner></banner>
+    <div class="row">
+      <div class="col-xs-12 col-xs-offset-6 col-md-offset-6">
+        <div class="box">
+          <button-game class="home__button"></button-game>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import Banner from '@/components/Banner.vue';
+import ButtonGame from '@/components/ButtonGame.vue';
 
 export default {
   name: 'home',
-  components: {}
+  components: {
+      Banner,
+      ButtonGame
+  }
 }
 </script>
+
+<style scoped lang="scss">
+@import '../styles/mixins/_media-queries';
+
+.home__button {
+  @include xs {
+    top: 80vh;
+  }
+  @include sm {
+    top: 82vh;
+  }
+  @include md {
+    top: 90vh;
+  }
+  top: 70vh;
+}
+</style>
